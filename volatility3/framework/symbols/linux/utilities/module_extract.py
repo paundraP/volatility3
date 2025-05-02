522c2d435c7a38a5d37b4afd8180a46f96eef779
Abyss Watcher <abyss-w4tcher@proton.me>
2025-05-02 14:36:20 +0200

    rollback to already patched version

--- volatility3/framework/symbols/linux/utilities/module_extract.py
+++ volatility3/framework/symbols/linux/utilities/module_extract.py
@@ -38,7 +38,7 @@ vollog = logging.getLogger(__name__)
 class ModuleExtract(interfaces.configuration.VersionableInterface):
     """Extracts Linux kernel module structures into an analyzable ELF file"""
 
-    _version = (1, 0, 2)
+    _version = (1, 0, 1)
     _required_framework_version = (2, 25, 0)
 
     framework.require_interface_version(*_required_framework_version)
