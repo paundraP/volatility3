7ed7d43c424010a9e5e1875854189299a5c52cc8
SolitudePy <47316655+SolitudePy@users.noreply.github.com>
2025-06-23 10:18:47 +0300

    plugins: bump lsof minor version

--- volatility3/framework/plugins/linux/lsof.py
+++ volatility3/framework/plugins/linux/lsof.py
@@ -110,7 +110,7 @@ class Lsof(plugins.PluginInterface, timeliner.TimeLinerInterface):
     """Lists open files for each processes."""
 
     _required_framework_version = (2, 0, 0)
-    _version = (2, 0, 2)
+    _version = (2, 1, 0)
 
     @classmethod
     def get_requirements(cls) -> List[interfaces.configuration.RequirementInterface]:
